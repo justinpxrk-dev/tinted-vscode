fb2d92830647e27817cd09285d9ae58b1c551e71
Justin Park <192945419+justinpxrk-dev@users.noreply.github.com>
2026-05-15 15:09:21 -0700

    Scope vscode-base24 template

--- scripts/updateWithLatestThemes.js
+++ scripts/updateWithLatestThemes.js
@@ -2,47 +2,52 @@
  * This script creates the necessary entries in package.json based on
  * the themes in ./themes directory
  */
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const { parse } = require('jsonc-parser');
+const { parse } = require("jsonc-parser");
 
 const basePath = process.cwd();
-const packagePath = path.join(basePath, 'package.json');
-const themesPath = path.join(basePath, 'themes');
+const packagePath = path.join(basePath, "package.json");
+const themesPath = path.join(basePath, "themes");
 
 // Add ./themes to package.json to register the theme in vscode
 async function removeThemesfromPackageJson() {
-  const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+  const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
   packageJson.contributes.themes = [];
 
-  fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2), 'utf8');
+  fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2), "utf8");
 }
 
 // Add ./themes to package.json to register the theme in vscode
 async function addThemesToPackageJson(system) {
   const systemThemesPath = path.join(themesPath, system);
-  const packageJsonThemeObjects = fs.readdirSync(systemThemesPath).map(filename => {
-    const themeJson = parse(fs.readFileSync(path.join(systemThemesPath, filename), 'utf8'));
+  const packageJsonThemeObjects = fs
+    .readdirSync(systemThemesPath)
+    .map((filename) => {
+      const themeJson = parse(
+        fs.readFileSync(path.join(systemThemesPath, filename), "utf8"),
+      );
 
-    return { 
-      label: filename.replace(/(.+)\.json/, '$1'),
-      uiTheme: themeJson.type === 'light' ? 'hc-light' : 'vs-dark',
-      path: `./themes/${system}/${filename}`
-    };
-  });
+      return {
+        label: filename.replace(/(.+)\.json/, "$1"),
+        uiTheme: themeJson.type === "light" ? "vs" : "vs-dark",
+        path: `./themes/${system}/${filename}`,
+      };
+    });
 
-  const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-  packageJson.contributes.themes = [...packageJson.contributes.themes, ...packageJsonThemeObjects];
+  const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
+  packageJson.contributes.themes = [
+    ...packageJson.contributes.themes,
+    ...packageJsonThemeObjects,
+  ];
 
-  fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2), 'utf8');
+  fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2), "utf8");
 }
 
 async function main() {
-  await removeThemesfromPackageJson("base16");
-  await addThemesToPackageJson("base16");
+  await removeThemesfromPackageJson();
   await addThemesToPackageJson("base24");
-  await addThemesToPackageJson("tinted8");
 }
 
 main();
